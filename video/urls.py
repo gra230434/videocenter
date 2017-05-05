bc3854a8e277fc1511b404a7c57a2fcf24ee4976
Kevin W <gra230434@gmail.com>
2017-05-05 20:30:48 +0800

    login page ver5

--- video/urls.py
+++ video/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include('index.urls')),
-    url(r'^videocenter/', include('videocenter.urls')),
+    url(r'^vcenter/', include('videocenter.urls')),
     url(r'^admin/', admin.site.urls),
     ]
--- videocenter/urls.py
+++ videocenter/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='videocenter_index'),
+    url(r'^$', views.index, name='vcenter_index'),
     ]
