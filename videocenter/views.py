f98f5f647776d637d1bfce6bd7f31324a6afcf77
Kevin W <gra230434@gmail.com>
2017-05-06 00:36:32 +0800

    try and error

--- videocenter/views.py
+++ videocenter/views.py
@@ -5,10 +5,13 @@ from django.http import HttpResponseRedirect
 # Create your views here.
 def index(request):
     if request.user.is_authenticated():
+        context = {'username': 'username'}
         if request.user.get_full_name() is not None:
-            context = {'username': request.user.get_full_name()}
+            context['username'] = request.user.get_full_name()
         else:
-            context = {'username': request.user.get_username()}
+            context['username'] = request.user.get_username()
+        context['account'] = request.user.get_username()
+        context['text'] = "Try and Error"
         return render(request, 'vcenter/index.html', context)
     else:
         return HttpResponseRedirect('/SingIn/')
