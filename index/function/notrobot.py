fb40f464db64045543975287ca2eb9d40a78962b
Kevin W <gra230434@gmail.com>
2017-05-09 03:11:05 +0800

    bug fix

--- index/function/notrobot.py
+++ index/function/notrobot.py
@@ -1,15 +1,16 @@
 import json
-import urllib
+import urllib.parse as parse
+import urllib.request as request
 
 from django.conf import settings
 
 
-def isrobot(request):
+def isrobot(requests):
     url = 'https://www.google.com/recaptcha/api/siteverify'
     values = {'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
-              'response': request}
-    data = urllib.parse.urlencode(values).encode()
-    req = urllib.request.Request(url, data=data)
-    response = urllib.request.urlopen(req)
+              'response': requests}
+    data = parse.urlencode(values).encode()
+    req = request.Request(url, data=data)
+    response = request.urlopen(req)
     result = json.loads(response.read().decode())
     return result
