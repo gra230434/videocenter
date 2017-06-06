fc87c77af39bcfb93348efe25cc9c1a5b0c9bd89
Kevin W <gra230434@gmail.com>
2017-06-07 00:37:16 +0800

    bug fix

--- MovieLens/urls.py
+++ MovieLens/urls.py
@@ -5,10 +5,10 @@ from . import views
 urlpatterns = [
     url(r'^$', views.MovieLens, name='MovieLens'),
     url(
-        r'^user/(?P<userID>[0-9]{6})/$',
+        r'^user/(?P<userID>[0-9]{,6})/$',
         views.MovieLensAUser, name='MovieLensUser'
         ),
     url(
-        r'^movie/(?P<movieID>[0-9]{6})/$',
+        r'^movie/(?P<movieID>[0-9]{,6})/$',
         views.MovieLensAMovie, name='MovieLensMovie'),
     ]
